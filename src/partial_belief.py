5cc029af63f4eb986dafc0e0ad4f7e623a22d067
Theomat <theomatricon@gmail.com>
2020-11-29 19:44:24 +0100

    Improved PartialBelief

--- src/partial_belief.py
+++ src/partial_belief.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List, Tuple
 
 import numpy as np
 
@@ -38,38 +38,25 @@ class PartialBelief:
         self.perspective_of = perspective_of
         self.players = players
         self.hand_size = 5 if players <= 3 else 4
+        self.hand = [{"rank": None, "color": None}] * self.hand_size
+        self.possible_colors = np.ones((self.hand_size, 5), dtype=np.int)
+        self.possible_ranks = np.ones((self.hand_size, 5), dtype=np.int)
 
-    def update(self, observation: Dict):
-        """
-        Update this belief according to the specified observations.
-
-        Parameters
-        -----------
-        - **observation**: the observation dictionnary from the perspective of ```observations_from```.
-        """
-        deck = _FULL_DECK_.copy()
-
-        # --------------------------------------
-        # CK PART
-        # --------------------------------------
+    def _update_ck(self, observation: Dict):
         # Remove cards from the discard pile
         for card in observation["discard_pile"]:
             color = _COLOR_TO_INDEX[card["color"]]
             rank = card["rank"]
-            deck[color, rank] -= 1
+            self.deck[color, rank] -= 1
 
         # Remove cards played
         for color, n in observation["fireworks"].items():
             cindex = _COLOR_TO_INDEX[color]
             for rank in range(n):
-                deck[cindex, rank] -= 1
-
+                self.deck[cindex, rank] -= 1
         # Note: hints are CK but are treated later
 
-        # --------------------------------------
-        # Partial knowledge part
-        # --------------------------------------
-
+    def _update_seen_hands(self, observation: Dict):
         # Update based on observed hands
         hands = observation["observed_hands"]
         offset = self.observation_from
@@ -81,13 +68,38 @@ class PartialBelief:
             for card in hand:
                 color = _COLOR_TO_INDEX[card["color"]]
                 rank = card["rank"]
-                deck[color, rank] -= 1
+                self.deck[color, rank] -= 1
             offset += 1
             offset %= self.players
 
-        # --------------------------------------
-        # Update based on hints
-        # --------------------------------------
+    def update_with_action(self, action: Dict):
+        if action["action_type"] == "PLAY" or action["action_type"] == "DISCARD":
+            offset = action["card_index"]
+            self.possible_ranks[offset:self.hand_size - 1, :] = self.possible_ranks[offset + 1:, :]
+            self.possible_ranks[self.hand_size - 1, :] = 1
+            self.possible_colors[offset:self.hand_size - 1, :] = self.possible_colors[offset + 1:, :]
+            self.possible_colors[self.hand_size - 1, :] = 1
+
+    def update_with_hint(self, action: Dict, hand: List[Dict]):
+        if action["action_type"] == "REVEAL_COLOR":
+            color = action["color"]
+            cindex = _COLOR_TO_INDEX[color]
+            for offset, card in enumerate(hand):
+                if card["color"] == color:
+                    self.possible_colors[offset, :] = 0
+                    self.possible_colors[offset, cindex] = 1
+                else:
+                    self.possible_colors[offset, cindex] = 0
+        elif action["action_type"] == "REVEAL_RANK":
+            rank = action["rank"]
+            for offset, card in enumerate(hand):
+                if card["rank"] == rank:
+                    self.possible_ranks[offset, :] = 0
+                    self.possible_ranks[offset, rank] = 1
+                else:
+                    self.possible_ranks[offset, rank] = 0
+
+    def _update_hints(self, observation: Dict):
         knowledge = observation["card_knowledge"]
 
         self.virtual_colors = np.zeros(5)
@@ -107,58 +119,105 @@ class PartialBelief:
             for card in obs_hand_kl:
                 color = _COLOR_TO_INDEX[card["color"]]
                 rank = card["rank"]
-                if color is None and rank == -1:
+                if color is None and rank is None:
                     continue
                 if color is None:
                     self.virtual_ranks[rank] += 1
-                elif rank == -1:
+                elif rank is None:
                     self.virtual_colors[color] += 1
                 else:
-                    deck[color, rank] -= 1
-        self.deck = deck
+                    self.deck[color, rank] -= 1
 
-    def _prob_color(self, card_info: Dict, color: int) -> float:
-        if color is None:
-            return None
+    def update(self, observation: Dict):
+        """
+        Update this belief according to the specified observations.
 
-        c = _COLOR_TO_INDEX[card_info["color"]]
-        if c == color:
-            return 1
-        elif c is not None:
+        Parameters
+        -----------
+        - **observation**: the observation dictionnary from the perspective of ```observations_from```.
+        """
+        self.deck = _FULL_DECK_.copy()
+
+        # --------------------------------------
+        # CK PART
+        # --------------------------------------
+        self._update_ck(observation)
+
+        # --------------------------------------
+        # Partial knowledge part
+        # --------------------------------------
+        self._update_seen_hands(observation)
+
+        deck_size = np.sum(self.deck) - self.hand_size * (1 + (self.perspective_of != self.observation_from))
+        deck_size = max(0, deck_size)
+        real_size = observation['deck_size']
+        assert deck_size == real_size, f"[Out of sync.] Invalid deck size:{deck_size} instead of {real_size}: {self.deck}"
+        # --------------------------------------
+        # Update based on hints
+        # --------------------------------------
+        # Must be done last
+        self._update_hints(observation)
+
+        assert len(np.where(self.deck < 0)[0]) == 0, "[Counting error] a card was removed one time too many !"
+
+    def _filtered_deck(self, offset: int) -> np.ndarray:
+        cards = self.deck.copy()
+        for r in range(5):
+            cards[:, r] *= self.possible_ranks[offset]
+        for c in range(5):
+            cards[c, :] *= self.possible_colors[offset]
+        return cards
+
+    def _total_cards(self, offset: int, color=False, rank=False) -> int:
+        cards = self.deck.copy()
+
+        for c in range(5):
+            cards[c, :] *= self.possible_colors[offset, c]
+
+        for r in range(5):
+            cards[:, r] *= self.possible_ranks[offset, r]
+
+        cards = np.sum(cards)
+        if rank:
+            cards -= np.sum(self.virtual_ranks * self.possible_ranks[offset])
+        if color:
+            cards -= np.sum(self.virtual_colors * self.possible_colors[offset])
+        return cards
+
+    def _prob_color(self, color: int, offset: int) -> float:
+        total_cards = self._total_cards(offset, color=True)
+        if total_cards == 0:
             return 0
-        total_cards = np.sum(self.deck) - np.sum(self.virtual_colors) + self.virtual_colors[color]
-        total_colors = np.sum(self.deck[color, :]) - self.virtual_colors[color]
+        total_colors = np.sum(self._filtered_deck(offset)[color, :]) - self.virtual_colors[color]
         return total_colors / total_cards
 
-    def _prob_rank(self, card_info: Dict, rank: int) -> float:
-        r = card_info["rank"]
-        if r == rank:
-            return 1
-        elif r is not None and r > -1:
+    def _prob_rank(self, rank: int,  offset: int) -> float:
+        total_cards = self._total_cards(offset, rank=True)
+        if total_cards == 0:
             return 0
-        total_cards = np.sum(self.deck) - np.sum(self.virtual_ranks) + self.virtual_ranks[rank]
-        total_rank = np.sum(self.deck[:, rank]) - self.virtual_ranks[rank]
-        return total_rank / total_cards
+        total_colors = np.sum(self._filtered_deck(offset)[:, rank]) - self.virtual_ranks[rank]
+        return total_colors / total_cards
 
-    def _prob_card_knowing_color(self, rank: int, color: int) -> float:
-        # p1 = P(C=r & R=r)
-        # p1 does not take into account hints
-        # p2 = P(C=c)
-        total_cards = np.sum(self.deck)
+    def _prob_card(self, rank: int, color: int, offset: int) -> float:
+        total_cards = self._total_cards(offset, color=False, rank=False)
+        # print("Total cards=", total_cards)
+        if total_cards == 0:
+            return 0
         total_in_deck = self.deck[color, rank]
-        p1 = total_in_deck / total_cards
-        p2 = self._prob_color({"color": None}, color)
-        return p1 / p2
+        return total_in_deck / total_cards
 
-    def _prob_card_knowing_rank(self, rank: int, color: int) -> float:
+    def _prob_card_knowing_color(self, rank: int, color: int, offset: int) -> float:
+        # p1 = P(C=r & R=r)
+        # p2 = P(C=c)
+        p1 = self._prob_card(rank, color, offset)
+        return p1
+
+    def _prob_card_knowing_rank(self, rank: int, color: int, offset: int) -> float:
         # p1 = P(C=r & R=r)
         # p1 does not take into account hints
         # p2 = P(R=R)
-        total_cards = np.sum(self.deck)
-        total_in_deck = self.deck[color, rank]
-        p1 = total_in_deck / total_cards
-        p2 = self._prob_rank({"rank": None}, rank)
-        return p1 / p2
+        p1 = self._prob_card(rank, color, offset)
+        return p1
 
     def probability(self, offset: int, rank: int = -1, color: str = None) -> float:
         """
@@ -183,9 +242,9 @@ class PartialBelief:
         if rank == -1:
             if color is None:
                 return 1
-            return self._prob_color(card_info, _COLOR_TO_INDEX[color])
+            return self._prob_color(_COLOR_TO_INDEX[color], offset)
         elif color is None:
-            return self._prob_rank(card_info, rank)
+            return self._prob_rank(rank, offset)
         else:
             r = card_info["rank"]
             c = card_info["color"]
@@ -193,12 +252,67 @@ class PartialBelief:
             if r == rank and c == color:
                 return 1
             elif c is None and rank == r:
-                return self._prob_card_knowing_rank(rank, cindex)
+                return self._prob_card_knowing_rank(rank, cindex, offset)
             elif c == color and r is None:
-                return self._prob_card_knowing_color(rank, cindex)
+                return self._prob_card_knowing_color(rank, cindex, offset)
             elif c is None and r is None:
-                total_cards = np.sum(self.deck)
-                total_in_deck = self.deck[cindex, rank]
-                return total_in_deck / total_cards
+                return self._prob_card(rank, cindex, offset)
             else:
                 return 0
+
+    def probability_playable(self, offset: int, fireworks: Dict) -> float:
+        probs = np.zeros(5)
+        for color, n in fireworks.items():
+            if n <= 4:
+                probs[_COLOR_TO_INDEX[color]] = self.probability(offset, n, color)
+        return np.sum(probs)
+
+    def most_informative_hint(self, actual_hand: List[Dict]) -> Tuple[Dict, float]:
+        ranks = []
+        colors = []
+        for card in actual_hand:
+            r, c = card["rank"], card["color"]
+            if r not in ranks:
+                ranks.append(r)
+            if c not in colors:
+                colors.append(c)
+
+        best_info = -1
+        best_rank = None
+        best_color = None
+        for rank in ranks:
+            info = 0
+            for offset, card in enumerate(actual_hand):
+                r, c = card["rank"], card["color"]
+                if r == rank:
+                    p = self.probability(offset, r, c)
+                    # print(f"P(rank={r}, color={c}, offset={offset})={p:.4f} possible ranks:", self.possible_ranks[offset], "possible colors:", self.possible_colors[offset])
+                    assert p > 0
+                    info += -np.log(p)
+            # print("Info of rank:", rank, f"info={info:.3f}")
+            if info > best_info:
+                best_info = info
+                best_rank = rank
+
+        for color in colors:
+            info = 0
+            for offset, card in enumerate(actual_hand):
+                r, c = card["rank"], card["color"]
+                if c == color:
+                    p = self.probability(offset, r, c)
+                    # print(f"P(rank={r}, color={c}, offset={offset})={p:.4f} possible ranks:", self.possible_ranks[offset], "possible colors:", self.possible_colors[offset])
+                    assert p > 0
+                    info += -np.log(p)
+            # print("Info of color:", color, f"info={info:.3f}")
+            if info > best_info:
+                best_info = info
+                best_rank = None
+                best_color = color
+
+        best_info /= np.log(2)
+        if best_rank is None:
+            if best_color is None:
+                raise Exception("No hint found at all !!")
+            return {"action_type": "REVEAL_COLOR", "color": best_color}, best_info
+        else:
+            return {"action_type": "REVEAL_RANK", "rank": best_rank}, best_info
