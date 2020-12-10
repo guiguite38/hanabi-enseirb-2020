cd5a5007ddc316e0678a4a473a37db48a5cd2d76
Mahaut <mmahaut@ensc.fr>
2020-12-10 12:13:48 +0100

    minor bug fix 7

--- environment/hanabi_learning_environment/agents/replay_memory.py
+++ environment/hanabi_learning_environment/agents/replay_memory.py
@@ -3,12 +3,15 @@ class ReplayMemory(object):
         self.capacity = capacity
         self.memory = []
         self.position = 0
+        self.Transition = namedtuple(
+            "Transition", ("state", "action", "next_state", "reward")
+        )
 
     def push(self, *args):
         """Saves a transition."""
         if len(self.memory) < self.capacity:
             self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
+        self.memory[self.position] = self.Transition(*args)
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
